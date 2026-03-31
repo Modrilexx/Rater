71398008b16de727ee60303e0631d18b6b9e2642
Modrilexx <mm2887@njit.edu>
2026-03-30 22:05:42 -0400

    Add animated ripple background

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,8 @@
-console.log("RATER exhibition loaded.");
+const bg = document.querySelector(".water-bg");
+
+window.addEventListener("scroll", () => {
+  const scroll = window.scrollY;
+
+  bg.style.transform =
+    `translate3d(${scroll * 0.05}px, ${scroll * 0.02}px, 0) scale(1.05)`;
+});
